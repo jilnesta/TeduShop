f7413634494d49822367a06f0ee0daaafffade70
FXPRO\fxpro <tran.vinhtoan@yahoo.com.vn>
2018-08-08 23:09:08 +0700

    no message

---
+++ TeduShop.Model/Abstract/Auditable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Abstract
+{
+    public abstract class Auditable :IAuditable
+    {
+        [MaxLength(256)]
+        public DateTime? CreateDate { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? UpdateDate { get; set; }
+        [MaxLength(256)]
+        public string UpdateBy { get; set; }
+
+        [MaxLength(256)]
+        public string MetaKeyWord { get; set; }
+        [MaxLength(256)]
+        public string MetaDescription { get; set; }
+
+        public bool Status { get; set; }
+    }
+}
---
+++ TeduShop.Model/Abstract/IAuditable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Abstract
+{
+    public interface IAuditable
+    { 
+        DateTime? CreateDate { get; set; }
+        string  CreatedBy { get; set; }
+        DateTime? UpdateDate { get; set; }
+        string UpdateBy { get; set; }
+
+        string MetaKeyWord { get; set; }
+        string MetaDescription { get; set; }
+
+        bool Status { get; set; }
+
+    }
+}
